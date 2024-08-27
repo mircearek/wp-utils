36a49ccd193d7ec869eb548195506e10c3e1b82d
Rechesan Mircea <mircearek@gmail.com>
2024-08-27 16:19:47 +0300

    Update old_builder_the_content_fix.php

--- old_builder_the_content_fix.php
+++ old_builder_the_content_fix.php
@@ -3,10 +3,22 @@
 add_filter('the_content', 'remove_fusion_shortcodes', 20, 1);
 
 function remove_fusion_shortcodes( $content ) {
-	$content = preg_replace('/\[\/?et_pb.*?\]/', '', $content);
-	return preg_replace_callback('/(?<!src=[\"\'])(https?:\/\/(.*?)\.(jpg|png|gif)(\?\w+=\w+)?)/i', '_callback', $content);
+
+    if ( is_single() && !is_front_page() && !is_home() ) {
+
+        // Remove the specified shortcodes
+        $content = preg_replace('/\[\/?et_pb.*?\]/', '', $content);
+
+        // Ensure responsive images are preserved
+        if ( $content ) {
+            return preg_replace_callback('/<img[^>]+src=[\"\'](https?:\/\/(.*?)\.(jpg|png|gif)(\?\w+=\w+)?)[\"\'][^>]*>/i', '_callback', $content);
+        }
+
+    }
+    return $content;
 }
 
-function _callback($matches){
-	return '<img src="'.$matches[0].'" />';
+function _callback($matches) {
+    // Return the entire matched <img> tag as is to preserve responsiveness
+    return $matches[0];
 }
