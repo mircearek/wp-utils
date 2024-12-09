46f4178eecaf4c5fccdfea6e436431e38139b2b3
Rechesan Mircea <mircearek@gmail.com>
2024-12-09 23:34:46 +0200

    scripts and styles support

--- clean_theme/start/includes/scripts_styles.php
+++ clean_theme/start/includes/scripts_styles.php
@@ -5,18 +5,24 @@ class Starter_Scripts_Styles {
 
 
     public function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'scripts_styles' ) );
 
     }
 
 
-    public function scripts() {
-
+    public function scripts_styles() {
+        wp_enqueue_style( 'style-child', get_stylesheet_directory_uri() . '/style.css' );
+        wp_enqueue_script( 'frontend-ajax', get_stylesheet_directory_uri() . '/js.js', array('jquery'), null, true );
+        wp_localize_script( 'frontend-ajax', 'frontend_ajax_object',
+            array( 
+                'ajaxurl' => admin_url( 'admin-ajax.php' ),
+                'data_var_1' => 'value 1',
+                'data_var_2' => 'value 2',
+            )
+        );
     }
 
 
-    public function styles() {
-        
-    }
 
 }
 
